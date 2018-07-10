9a837fc72cdb46446211a59a37c2ee9dfdeb1c9a
Madeline Eckhart <madelineaeckhart@gmail.com>
2018-07-10 12:25:52 -0500

    successful change material during runtime

---
+++ ARSceneKit/MaterialCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MaterialCollectionViewCell.swift
+//  ARSceneKit
+//
+//  Created by Madeline Eckhart on 7/10/18.
+//  Copyright © 2018 Esteban Herrera. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MaterialCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
+}
--- ARSceneKit/ViewController.swift
+++ ARSceneKit/ViewController.swift
@@ -16,15 +16,19 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, UI
     @IBOutlet var sceneView: ARSCNView!
     @IBOutlet var collectionView: UICollectionView!
     @IBOutlet weak var searchingLabel: UILabel!
+
+    //----------------------------------------------- Node and Scene Setup -----------------------------------------------//
     
-    private var nodeModel:SCNNode!
-    private var tempMaterial: SCNMaterial = SCNMaterial()
+    private var nodeModel:SCNNode?
     var planeNode: PlaneDetectionNode?
+    var nodePlaced: Bool = false
+    
     private var screenCenter: CGPoint!
     private var selectedNode: SCNNode?
     private var originalRotation: SCNVector3?
     private var originalSize: SCNVector3?
     let nodeName = "makeupScene"
+    
     
     let session = ARSession()
     let sessionConfiguration: ARWorldTrackingConfiguration = {
@@ -33,13 +37,14 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, UI
         return config
     }()
     
+   //----------------------------------------------- View Before Loading -----------------------------------------------//
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         sceneView.delegate = self
         sceneView.session = session
-        selectedNode?.castsShadow = true
-        
+
         // Lighting
         sceneView.automaticallyUpdatesLighting = true
         sceneView.autoenablesDefaultLighting = true
@@ -50,14 +55,8 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, UI
         screenCenter = view.center
         
         if let modelScene = SCNScene(named:"perfume.scn") {
-            //nodeModel =  modelScene.rootNode.childNode(withName: nodeName, recursively: true)
-            nodeModel = modelScene.rootNode
-            
-            tempMaterial.diffuse.contents = UIImage(named: "front")
-            let otherMaterial = SCNMaterial()
-            otherMaterial.diffuse.contents = UIColor.blue
-            nodeModel.geometry?.materials = [tempMaterial, otherMaterial, otherMaterial, otherMaterial, otherMaterial, otherMaterial]
-            
+            nodeModel = modelScene.rootNode.childNode(withName: nodeName, recursively: true)
+            nodeModel?.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
         }else{
             print("can't load model")
         }
@@ -91,113 +90,86 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, UI
         }
     }
     
-    // Product Collection View
-    // var products: [ProductList] = []
+   //----------------------------------------------- Product Collection View -----------------------------------------------//
+
     var materials: [UIImage] = []
     
     func numberOfSections(in collectionView: UICollectionView) -> Int {
-        /* list of products
-        let mac: ProductList = ProductList(newName: "mac", newImage: UIImage(named: "mac_palette_material")!)
-        let foundation: ProductList = ProductList(newName: "foundation", newImage: UIImage(named: "mac_fix_material")!)
-        let perfume: ProductList = ProductList(newName: "perfume", newImage: UIImage(named: "front")!)
-        products.append(mac)
-        products.append(foundation)
-        products.append(perfume)
-         */
+
         // list of materials
-        materials.append(#imageLiteral(resourceName: "purple"))
+        materials.append(UIImage(named: "purple")!)
         materials.append(#imageLiteral(resourceName: "green"))
         materials.append(#imageLiteral(resourceName: "salmon"))
+        materials.append(#imageLiteral(resourceName: "front"))
         return 1
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        //return products.count
+
         return materials.count
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! ProductCollectionViewCell
-        //let image = products[indexPath.row].image
-        let image = materials[indexPath.row]
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MaterialCollectionViewCell
+        let image:UIImage = materials[indexPath.row]
         cell.imageView.image = image
-        //cell.layer.cornerRadius = 4
+        cell.layer.cornerRadius = 4
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        
         // cell highlight when selected
         let cell = collectionView.cellForItem(at: indexPath)
         cell?.layer.borderWidth = 2.0
         cell?.layer.borderColor = UIColor.red.cgColor
         
-        
-        /* choose product
-        if products[indexPath.row].name == "mac" {
-            if let modelScene = SCNScene(named:"mac_palette.scn") {
-                self.nodeModel =  modelScene.rootNode.childNode(withName: self.nodeName, recursively: true)
-                print("got product")
-            }
-            else {
-                print("can't load model")
-            }
-        }
-        if products[indexPath.row].name == "foundation" {
-            if let modelScene = SCNScene(named:"mac_fix.scn") {
-                self.nodeModel =  modelScene.rootNode.childNode(withName: self.nodeName, recursively: true)
-                print("got product")
-            }
-            else {
-                print("can't load model")
-            }
-        }
-        if products[indexPath.row].name == "perfume" {
-            if let modelScene = SCNScene(named:"perfume.scn") {
-                self.nodeModel =  modelScene.rootNode.childNode(withName: self.nodeName, recursively: true)
-                print("got product")
-            }
-            else {
-                print("can't load model")
-            }
-        }
-         */
-
+        let image = materials[indexPath.row]
+        nodeModel?.geometry?.firstMaterial?.diffuse.contents = image
     }
-    
+
     func collectionView(_ collectionView: UICollectionView, didDeselectItemAt indexPath: IndexPath) {
+        
         // cell un-highlight when selected
         let cell = collectionView.cellForItem(at: indexPath)
         cell?.layer.borderWidth = 0.0
     }
 
-    // Recognizing Gestures
+   //----------------------------------------------- Recognizing Gestures -----------------------------------------------//
+    
     private func node(at position: CGPoint) -> SCNNode? {
+        
         return sceneView.hitTest(position, options: nil)
             .first(where: { $0.node !== planeNode && $0.node !== nodeModel })?
             .node
     }
     
     @objc private func viewTapped(_ gesture: UITapGestureRecognizer) {
-        // Make sure we've found the floor
-        guard planeNode != nil else { return }
         
-        // See if we tapped on a plane where a model can be placed
-        let results = sceneView.hitTest(screenCenter, types: .existingPlane)
-        guard let transform = results.first?.worldTransform else { return }
-        
-        // Find the position to place the model
-        let position = float3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
-        
-        // Create a copy of the model set its position/rotation
-        let newNode = nodeModel.flattenedClone()
-        newNode.simdPosition = position
-        
-        // Add the model to the scene
-        sceneView.scene.rootNode.addChildNode(newNode)
-        
+        if nodePlaced == false {
+            // Make sure we've found the floor
+            guard planeNode != nil else { return }
+            
+            // See if we tapped on a plane where a model can be placed
+            let results = sceneView.hitTest(screenCenter, types: .existingPlane)
+            guard let transform = results.first?.worldTransform else { return }
+            
+            // Find the position to place the model
+            let position = float3(transform.columns.3.x, transform.columns.3.y, transform.columns.3.z)
+            
+            // Create a copy of the model set its position/rotation
+            let newNode = nodeModel?.flattenedClone()
+            newNode?.simdPosition = position
+            
+            // Add the model to the scene
+            sceneView.scene.rootNode.addChildNode(newNode!)
+            nodePlaced = true
+        }
     }
     
     @objc private func viewPanned(_ gesture: UIPanGestureRecognizer) {
+        
         // Find the location in the view
         let location = gesture.location(in: sceneView)
         
@@ -218,6 +190,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, UI
     }
     
     @objc private func viewRotated(_ gesture: UIRotationGestureRecognizer) {
+        
         let location = gesture.location(in: sceneView)
         
         guard let node = node(at: location) else { return }
@@ -235,6 +208,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, UI
     }
     
     @objc private func viewPinched(_ gesture: UIPinchGestureRecognizer) {
+        
         let location = gesture.location(in: sceneView)
         
         guard let node = node(at: location) else { return }
@@ -250,14 +224,15 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, UI
         }
     }
     
-    // Plane Detection
+   //----------------------------------------------- Plane Detection -----------------------------------------------//
+    
     func renderer(_ renderer: SCNSceneRenderer, didAdd node: SCNNode, for anchor: ARAnchor) {
         
         guard planeNode == nil else { return }
         
         // Create a new focal node
         let node = PlaneDetectionNode()
-        node.addChildNode(nodeModel)
+        node.addChildNode(nodeModel!)
         
         //sceneView.scene.rootNode.addChildNode(node)
         self.planeNode = node
@@ -275,6 +250,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, UI
     }
     
     func renderer(_ renderer: SCNSceneRenderer, updateAtTime time: TimeInterval) {
+        
         // If we haven't established a focal node yet do not update
         guard let planeNode = planeNode else { return }
         
@@ -290,20 +266,22 @@ class ViewController: UIViewController, ARSCNViewDelegate, ARSessionDelegate, UI
     
     // Override to create and configure nodes for anchors added to the view's session.
     func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+        
         let node = SCNNode()
-     
         return node
     }
+
+   //----------------------------------------------- View After Loading -----------------------------------------------//
     
     override func viewWillDisappear(_ animated: Bool) {
-        // Pause ARKit while the view is gone
+        
         session.pause()
-            
         super.viewWillDisappear(animated)
 
     }
 
     override func didReceiveMemoryWarning() {
+        
         super.didReceiveMemoryWarning()
         // Release any cached data, images, etc that aren't in use.
     }
